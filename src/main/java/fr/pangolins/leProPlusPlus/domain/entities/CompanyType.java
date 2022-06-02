e77532baa107a4c50df336cebe89c894a1ff6890
leo gravier <gravierleo@live.fr>
2022-06-02 09:21:32 +0200

    prepare entity for db change

--- src/main/java/fr/pangolins/leProPlusPlus/domain/entities/CompanyType.java
+++ src/main/java/fr/pangolins/leProPlusPlus/domain/entities/CompanyType.java
@@ -3,11 +3,7 @@ package fr.pangolins.leProPlusPlus.domain.entities;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document("companyTypes")
 public class CompanyType {
-    @Id
-    private String id;
-
     private String name;
 
     public CompanyType() {
@@ -16,14 +12,6 @@ public class CompanyType {
 
     public CompanyType(String name) {
         this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public String getName() {
--- src/main/java/fr/pangolins/leProPlusPlus/domain/entities/Notice.java
+++ src/main/java/fr/pangolins/leProPlusPlus/domain/entities/Notice.java
@@ -1,32 +1,37 @@
 package fr.pangolins.leProPlusPlus.domain.entities;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("notices")
 public class Notice {
     @Id
-    private String id;
+    private ObjectId id;
 
     private String title;
     private String content;
     private float mark;
+    @DBRef()
+    private Company author;
 
     public Notice() {
         //
     }
 
-    public Notice(String title, String content, float mark) {
+    public Notice(String title, String content, float mark, Company author) {
         this.title = title;
         this.content = content;
         this.mark = mark;
+        this.author = author;
     }
 
-    public String getId() {
+    public ObjectId getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(ObjectId id) {
         this.id = id;
     }
 
@@ -52,5 +57,13 @@ public class Notice {
 
     public void setMark(float mark) {
         this.mark = mark;
+    }
+
+    public Company getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Company author) {
+        this.author = author;
     }
 }
