e1cd110351507ba54dcb59e700c5e5d6a95af7ba
Sohar-FV <florian.vivet@gmail.com>
2022-06-09 15:22:10 +0200

    tests companies

--- src/test/java/fr/pangolins/leProPlusPlus/CompaniesControllerTests.java
+++ src/test/java/fr/pangolins/leProPlusPlus/CompaniesControllerTests.java
@@ -1,36 +1,59 @@
 package fr.pangolins.leProPlusPlus;
 
+import fr.pangolins.leProPlusPlus.domain.entities.Company;
+import fr.pangolins.leProPlusPlus.repository.CompanyRepository;
+import fr.pangolins.leProPlusPlus.responses.CompanyResponse;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.Stack;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CompaniesControllerTests {
-    private RestTemplate restTemplate;
     private String url;
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private CompanyRepository companyRepository;
 
     @LocalServerPort
     private int port;
-
-    private final Stack<String> bankAccountsIds = new Stack<>();
 
     @BeforeEach
     public void beforeEach() {
         url = "http://localhost:" + port + "/companies";
         restTemplate = new RestTemplate();
     }
+    @Test
+    public void getAllIsOK() throws Exception {
+         ResponseEntity response = restTemplate.exchange(url, HttpMethod.GET,null , List.class);
+         System.out.println(response.getStatusCode());
+         assertEquals(response.getStatusCode(), HttpStatus.OK);
+    }
 
     @Test
-    public void stubTest() {
-        assertTrue(true);
+    public void createIsOk() throws Exception {
+        //Given
+        String requestParams = "?name=company1";
+
+        //When
+        ResponseEntity response = restTemplate.exchange(url + requestParams, HttpMethod.POST,null , CompanyResponse.class);
+
+        //Then
+        assertEquals(response.getStatusCode(), HttpStatus.CREATED);
+        assertEquals(((CompanyResponse) response.getBody()).getName(), "company1");
     }
 }
